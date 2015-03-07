2a84a9d6cd497990def8ce528dfa1798adec1fa2
Anton <abardakhan@gmail.com>
2015-03-06 19:39:43 -0800

    format

--- gameengine.js
+++ gameengine.js
@@ -1,13 +1,13 @@
 // This game shell was happily copied from Googler Seth Ladd's "Bad Aliens" game and his Google IO talk in 2011
-window.requestAnimFrame = (function () {
+window.requestAnimFrame = (function() {
   return window.requestAnimationFrame ||
-  window.webkitRequestAnimationFrame ||
-  window.mozRequestAnimationFrame ||
-  window.oRequestAnimationFrame ||
-  window.msRequestAnimationFrame ||
-  function (/* function */ callback, /* DOMElement */ element) {
-    window.setTimeout(callback, 1000 / 60);
-  };
+    window.webkitRequestAnimationFrame ||
+    window.mozRequestAnimationFrame ||
+    window.oRequestAnimationFrame ||
+    window.msRequestAnimationFrame ||
+    function( /* function */ callback, /* DOMElement */ element) {
+      window.setTimeout(callback, 1000 / 60);
+    };
 })();
 
 
@@ -17,7 +17,7 @@ function Timer() {
   this.wallLastTimestamp = 0;
 }
 
-Timer.prototype.tick = function () {
+Timer.prototype.tick = function() {
   var wallCurrent = Date.now();
   var wallDelta = (wallCurrent - this.wallLastTimestamp) / 1000;
   this.wallLastTimestamp = wallCurrent;
@@ -40,7 +40,7 @@ function GameEngine() {
   this.gameover = 0;
 }
 
-GameEngine.prototype.init = function (ctx) {
+GameEngine.prototype.init = function(ctx) {
   this.ctx = ctx;
   this.surfaceWidth = this.ctx.canvas.width;
   this.surfaceHeight = this.ctx.canvas.height;
@@ -49,19 +49,19 @@ GameEngine.prototype.init = function (ctx) {
   console.log('game initialized');
 }
 
-GameEngine.prototype.start = function () {
-	var audio = new Audio('sound/Game of Thrones.mp3');
-	audio.loop = true;
-	audio.play();
-	console.log("starting game");
-	var that = this;
-	(function gameLoop() {
-		that.loop();
-		requestAnimFrame(gameLoop, that.ctx.canvas);
+GameEngine.prototype.start = function() {
+  var audio = new Audio('sound/Game of Thrones.mp3');
+  audio.loop = true;
+  audio.play();
+  console.log("starting game");
+  var that = this;
+  (function gameLoop() {
+    that.loop();
+    requestAnimFrame(gameLoop, that.ctx.canvas);
   })();
 }
 
-GameEngine.prototype.startInput = function () {
+GameEngine.prototype.startInput = function() {
   console.log('Starting input');
   var timerX = 0;
   var period = 0;
@@ -69,35 +69,33 @@ GameEngine.prototype.startInput = function () {
 
   var that = this;
 
-  this.ctx.canvas.addEventListener("keydown", function (e) {
+  this.ctx.canvas.addEventListener("keydown", function(e) {
     if (String.fromCharCode(e.which) === ' ') {
 
 
       var that = this;
 
       setInterval(function() {
-        if(timerX < amount_in_wave && period == 0) {
+        if (timerX < amount_in_wave && period == 0) {
           var troll = new Troll();
           enemies.push(troll);
           timerX++;
-        }
-        else if(timerX < amount_in_wave && period == 1) {
+        } else if (timerX < amount_in_wave && period == 1) {
           var grunt = new Grunt();
           enemies.push(grunt);
           timerX++;
-        }
-        else if(timerX < amount_in_wave && period == 2){
+        } else if (timerX < amount_in_wave && period == 2) {
           var ogre = new Ogre();
           enemies.push(ogre);
           timerX++;
         }
-        if(timerX == amount_in_wave)  {
+        if (timerX == amount_in_wave) {
           timerX = 500000;
-          setTimeout(function () {
+          setTimeout(function() {
 
             timerX = 0;
-            if(period == 3) {
-              setTimeout(function () {
+            if (period == 3) {
+              setTimeout(function() {
                 period = 0;
                 amount_in_wave = amount_in_wave + 3;
               }, 10000);
@@ -116,27 +114,26 @@ GameEngine.prototype.startInput = function () {
       // var ogre = new Ogre();
       // enemies.push(ogre);
       // console.log(enemies);
-    }else if (String.fromCharCode(e.which) === 'g') {
+    } else if (String.fromCharCode(e.which) === 'g') {
       var grunt = new Grunt();
       enemies.push(grunt);
       //2console.log(enemies);
-    }
-    else if (String.fromCharCode(e.which) === 't') {
+    } else if (String.fromCharCode(e.which) === 't') {
       var troll = new Troll();
       enemies.push(troll);
       console.log(enemies);
     } else if (String.fromCharCode(e.which) === '1') {
-		if (money >= 100) {
-			buildmode = 1;
-		}
+      if (money >= 100) {
+        buildmode = 1;
+      }
     } else if (String.fromCharCode(e.which) === '2') {
-		if (money >= 250) {
-			buildmode = 2;
-		}
+      if (money >= 250) {
+        buildmode = 2;
+      }
     } else if (String.fromCharCode(e.which) === '3') {
-		if (money >= 150) {
-			buildmode = 3;
-		}
+      if (money >= 150) {
+        buildmode = 3;
+      }
     } else if (e.which === 27) {
       that.gameover = that.gameover === 1 ? 0 : 1;
       console.log('gameover=' + that.gameover);
@@ -146,20 +143,23 @@ GameEngine.prototype.startInput = function () {
     e.preventDefault();
   }, false);
 
-  var getXandY = function (e) {
+  var getXandY = function(e) {
     var x = e.clientX - that.ctx.canvas.getBoundingClientRect().left;
     var y = e.clientY - that.ctx.canvas.getBoundingClientRect().top;
     //x= Math.floor(x / 65);
     //y = Math.floor(y / 65);
-    return { x: x, y: y };
+    return {
+      x: x,
+      y: y
+    };
   }
 
-  this.ctx.canvas.addEventListener("mousemove", function (e) {
+  this.ctx.canvas.addEventListener("mousemove", function(e) {
     //console.log(getXandY(e));
     that.mouse = getXandY(e);
   }, false);
 
-  this.ctx.canvas.addEventListener("click", function (e) {
+  this.ctx.canvas.addEventListener("click", function(e) {
     //console.log(getXandY(e));
     that.click = getXandY(e);
   }, false);
@@ -168,12 +168,12 @@ GameEngine.prototype.startInput = function () {
   console.log('Input started');
 }
 
-GameEngine.prototype.addEntity = function (entity) {
+GameEngine.prototype.addEntity = function(entity) {
   console.log('added entity');
   this.entities.push(entity);
 }
 
-GameEngine.prototype.draw = function () {
+GameEngine.prototype.draw = function() {
   this.ctx.clearRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
   this.ctx.save();
   for (var i = 0; i < this.entities.length; i++) {
@@ -182,7 +182,7 @@ GameEngine.prototype.draw = function () {
   this.ctx.restore();
 }
 
-GameEngine.prototype.update = function () {
+GameEngine.prototype.update = function() {
   var entitiesCount = this.entities.length;
 
   for (var i = 0; i < entitiesCount; i++) {
@@ -200,8 +200,8 @@ GameEngine.prototype.update = function () {
   }
 }
 
-GameEngine.prototype.loop = function () {
-  if (this.gameover === 0){
+GameEngine.prototype.loop = function() {
+  if (this.gameover === 0) {
     this.clockTick = this.timer.tick();
     this.update();
     this.draw();
@@ -220,10 +220,9 @@ function Entity(game, x, y) {
   this.removeFromWorld = false;
 }
 
-Entity.prototype.update = function () {
-}
+Entity.prototype.update = function() {}
 
-Entity.prototype.draw = function (ctx) {
+Entity.prototype.draw = function(ctx) {
   if (this.game.showOutlines && this.radius) {
     this.game.ctx.beginPath();
     this.game.ctx.strokeStyle = "green";
@@ -233,7 +232,7 @@ Entity.prototype.draw = function (ctx) {
   }
 }
 
-Entity.prototype.rotateAndCache = function (image, angle) {
+Entity.prototype.rotateAndCache = function(image, angle) {
   var offscreenCanvas = document.createElement('canvas');
   var size = Math.max(image.width, image.height);
   offscreenCanvas.width = size;
